abd34dffbe4baa5f27b3fcffb36e42400cc6e262
Vladimir Starkov <iamstarkov@gmail.com>
2017-06-01 19:25:13 +0200

    expose themeListener

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const createThemeProvider = require('./create-theme-provider');
 const createWithTheme = require('./create-with-theme');
+const createThemeListener = require('./create-theme-listener');
 const channel = require('./channel');
 
 module.exports = {
   channel,
   withTheme: createWithTheme(),
   ThemeProvider: createThemeProvider(),
+  themeListener: createThemeListener(),
   createTheming: (customChannel = channel) => ({
     channel: customChannel,
     withTheme: createWithTheme(customChannel),
     ThemeProvider: createThemeProvider(customChannel),
+    themeListener: createThemeListener(customChannel),
   }),
 };
